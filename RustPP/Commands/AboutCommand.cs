4db65d4377c156f06c303db8fcff40ff5350a6b1
enes35001 <31064949+enes35001@users.noreply.github.com>
2018-12-03 08:04:17 +0300

    Update AboutCommand.cs

--- RustPP/Commands/AboutCommand.cs
+++ RustPP/Commands/AboutCommand.cs
@@ -9,8 +9,8 @@
         public override void Execute(ref ConsoleSystem.Arg Arguments, ref string[] ChatArguments)
         {
             var pl = Fougerite.Server.Cache[Arguments.argUser.userID];
-            pl.MessageFrom(Core.Name, "Fougerite is currently running Rust++ v" + Core.Version);
-            pl.MessageFrom(Core.Name, "Brought to you by xEnt & EquiFox17 & the Fougerite project.");
+            pl.MessageFrom(Core.Name, "Fougerite Şuanda Rust++ Sistemiyle Çalışıyor Versiyon" + Core.Version);
+            pl.MessageFrom(Core.Name, "Türkçe Yama Enes Şevluk Trafından Yapılmış Yaratıcı ise DreTaX");
         }
     }
 }
